787476a50e5e13845c879d599fdcf88bb0201e0c
kisshan13 <kisshan07@outlook.com>
2023-05-22 11:06:52 +0530

    added user creation and sessions

--- prisma/db.js
+++ prisma/db.js
@@ -1,3 +1,89 @@
 import { PrismaClient } from '@prisma/client'
+import { hash } from '../lib/utils'
+import jwt from 'jsonwebtoken'
+import ck from 'ckey'
 
 export const prisma = new PrismaClient()
+
+export async function addUser({ name, email, password }) {
+    try {
+        const isUserExist = await prisma.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+
+        if (isUserExist) {
+            return {
+                success: false,
+                error: 'Email already exists.'
+            }
+        }
+
+        const user = await prisma.user.create({
+            data: {
+                email: email,
+                name: name,
+                password: hash(password)
+            }
+        })
+
+        const session = await createSession({
+            email: user.email,
+            id: user.id,
+            name: user.name
+        })
+
+        return session
+    }
+
+    catch (e) {
+        return {
+            success: false,
+            error: 'Internal server error.'
+        }
+    }
+}
+
+export async function createSession({ email, id, name }) {
+    try {
+        const refreshToken = jwt.sign({
+            token: 'refresh_token'
+        }, ck.JWT_SECRET, {
+            expiresIn: 60 * 60 * 24 * 100,
+            issuer: 'Mock-API',
+            subject: 'Refresh'
+        })
+
+        await prisma.session.create({
+            data: {
+                id: id,
+                refresh_token: refreshToken
+            }
+        })
+
+        const token = jwt.sign({
+            email: email,
+            id: id,
+            name: name
+        }, ck.JWT_SECRET, {
+            expiresIn: 60 * 20
+        })
+
+        return {
+            success: true,
+            data: {
+                token: token,
+                refresh_token: refreshToken
+            }
+        }
+
+    }
+
+    catch (e) {
+        return {
+            success: false,
+            error: 'Session creation error'
+        }
+    }
+}
