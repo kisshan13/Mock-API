9d413ec6e54610822844c728aebfa76dcd59731e
kisshan13 <kisshan07@outlook.com>
2023-05-23 17:43:48 +0530

    edited db.js

--- prisma/db.js
+++ prisma/db.js
@@ -1,37 +1,3 @@
 import { PrismaClient } from '@prisma/client'
-import { hash } from '../lib/utils.js'
-import jwt from 'jsonwebtoken'
-import ck from 'ckey'
 
 export const prisma = new PrismaClient()
-
-export async function addData({ id, info }) {
-    try {
-        const data = await prisma.data.upsert({
-            where: {
-                userId: id
-            },
-
-            update: {
-                data: info
-            },
-
-            create: {
-                data: info,
-                userId: id
-            }
-        })
-
-        return {
-            success: true,
-            data: data
-        }
-    }
-
-    catch (e) {
-        return {
-            success: false,
-            error: 'Internal server error'
-        }
-    }
-}
